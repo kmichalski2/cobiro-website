351840d6822772812e7034a86a8535da9148ba97
Mads Røjkum <roejkum@gmail.com>
2019-09-09 11:10:30 +0200

    featuredCarousel: finalize style and functionality

--- src/components/sections/featuredCarousel/featuredCarouselSlide/featuredCarouselSlide.js
+++ src/components/sections/featuredCarousel/featuredCarouselSlide/featuredCarouselSlide.js
@@ -2,14 +2,21 @@ import React from "react"
 
 import "./featuredCarouselSlide.scss"
 
+import arrow from '../../../../images/arrow_blue_up.svg'
+import icon from '../../../../images/placeholder_icon_nobg.svg'
+
 
 const FeaturedCarouselSlide = ( props ) => {
     
     return (
         <li onClick={props.click} className={props.active ? 'active' : ''}>
-            <h4>{props.title}</h4>
+            <h4> 
+                <img src={icon} className="icon" alt={[props.title, 'icon'].join(' ')}/>
+                {props.title}
+                <img src={arrow} className="arrow" alt={[props.title, 'arrow'].join(' ')}/>
+            </h4>
             <div className={props.active ? 'open slide-text' : 'slide-text'}>
-                <p>{props.text}</p>
+                <p className="small">{props.text}</p>
             </div>
         </li>
 
