e5177b927f402703d2785b5d641a5050ad155387
Mads Røjkum <roejkum@gmail.com>
2019-11-04 15:44:38 +0100

    fix image waves

--- src/components/sections/image/image.js
+++ src/components/sections/image/image.js
@@ -5,7 +5,7 @@ import ImageStyles from "./image.module.scss"
 const Image = ({ data }) => {
     return (
         <section className={ImageStyles.section}>
-            <svg className={ImageStyles.topSway} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 61.88"><g id="Lag_2" data-name="Lag 2"><g id="Lag_1-2" data-name="Lag 1"><path id="Path_5024" data-name="Path 5024" d="M1920,28.54S1725,0,1387.75,0,816.35,61.88,456.07,61.88,0,48.16,0,48.16V0H1920Z" fill="#fff"/></g></g></svg>
+            <svg className={ImageStyles.topSway} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 61" preserveAspectRatio="xMidYMid slice" x="0" y="0" ><path d="M1920,28.54S1725,0,1387.75,0,816.35,61,456.07,61,0,48.16,0,48.16V0H1920Z" fill="#fff"/></svg>
             {data.image.fluid ?
                 <Img className="img-full-width"
                 fluid={data.image.fluid}
@@ -17,7 +17,7 @@ const Image = ({ data }) => {
                 alt={data.image.alt ? data.image.alt : data.title}
                 />
             }
-            <svg className={ImageStyles.bottomSway} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 61.88"><g id="Lag_2" data-name="Lag 2"><g id="Lag_1-2" data-name="Lag 1"><path id="Path_5024" data-name="Path 5024" d="M0,33.35S195,61.88,532.25,61.88,1103.65,0,1463.93,0,1920,13.72,1920,13.72V61.88H0Z" fill="#fff"/></g></g></svg>
+            <svg className={ImageStyles.bottomSway} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 61" preserveAspectRatio="xMidYMid slice" x="0" y="0" ><path d="M0,33.35S195,61,532.25,61,1103.65,0,1463.93,0,1920,13.72,1920,13.72V61H0Z" fill="#fff"/></svg>
         </section>
     )
 }
