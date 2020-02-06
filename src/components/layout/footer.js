e8570a790a2dc5766b69eed06ae537e8350924dc
Malthe Blichfeldt <maltheblichfeldt@gmail.com>
2020-02-06 12:44:50 +0100

    fix

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -28,6 +28,12 @@ const Footer = ({ columns }) => {
            <ul className="list-unstyled menu">
              {col.column.map((el, index) => (
                <li key={index} className={el.text ? 'text-darkgrey' : null}>
+               { /* el.googlePartnerLogo ?
+                <div className="googlePartnerLogo">
+                  <a href={el.externalLink} target="_blank" rel="noopener noreferrer">
+                    <GoogleLogo/>
+                  </a>
+               </div> */}
                 {el.text ?
                   el.text
                  : el.image ?
