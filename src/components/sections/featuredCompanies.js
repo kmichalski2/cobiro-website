b41329008d36e2f38a7182c27a413170ce96b12b
Malthe Blichfeldt <maltheblichfeldt@gmail.com>
2020-02-06 13:00:33 +0100

    remove outcommented 3

--- src/components/sections/featuredCompanies.js
+++ src/components/sections/featuredCompanies.js
@@ -5,7 +5,6 @@ import GoogleLogo from '../googleLogo/googleLogo'
 
 const FeaturedCompanies = ({ data }) => (
   <section className={[data.backgroundColor ? "bg-sway" : null, "section"].join(' ')} style={{ position: "relative" }}>
-    {/* <img src={splash} className="splash-left" alt="splash left" /> */}
     <div className={[data.backgroundColor ? "bg-sway-inner" : null, "section-inner"].join(' ')}>
       <div className="container">
         <div className="row center-xs middle-xs text-center">
@@ -14,7 +13,6 @@ const FeaturedCompanies = ({ data }) => (
             { data.text ? <p>{data.text}</p> : null }
           </div>
           <div className="col col-xs-12 col-lg-8 center">
-          { /*data.googlePartnerLogo ? <div style={{transform: "scale(0.8)"}}><GoogleLogo /></div> : null */ }
           {data.logos.map((l, index) => (
             l.fixed ? <Img key={index} className="customer-logo" fixed={l.fixed} alt={l.alt ? l.alt : `logo ${index + 1}`} />
             : <img src={l.url} alt={l.alt ? l.alt : `logo ${index + 1}`} />
