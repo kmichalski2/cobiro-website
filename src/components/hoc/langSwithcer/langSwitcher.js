3833290194260c467c8a0bbf70979e833ffd731a
Malthe Blichfeldt <maltheblichfeldt@gmail.com>
2020-02-20 15:19:30 +0100

    WIP langchanger (state not updating correctly yet)

--- src/components/hoc/langSwithcer/langSwitcher.js
+++ src/components/hoc/langSwithcer/langSwitcher.js
@@ -1,37 +1,18 @@
-import React, { useState, useEffect } from "react"
-import { changeLocale, useIntl } from 'gatsby-plugin-intl'
+// import React, { Link } from "react"
+// import langStyles from './langSwitcher.module.scss'
 
-import langStyles from './langSwitcher.module.scss'
+// const LanguageSwitcher = ({locales}) => {
 
-const LangSwitcher = ({ direction, styling }) => {
-    const langs = [
-        {lang: 'en', label: 'UK'},
-        {lang: 'da', label: 'DK'}
-    ]
-    
-    const currentLang = useIntl().locale
-    const [currentLangLabel, setCurrentLangLabel] = useState('')
 
-    useEffect(() => {
-        setCurrentLangLabel(currentLang === 'da' ? 'DK' : currentLang === 'nb' ? 'NO' : currentLang === 'en' ? 'UK' : '')
-    })
+//     return (
+//         <>
+//               {locales.map((l, i) =>
+//                   <Link key={i} to={`${l.locale === 'en' ? '/' : `/${l.locale}`}/${l.value}`}>
+//                     {l.locale}
+//                   </Link>)
+//               }
+//         </>
+//     )
+// }
 
-    const langSwitchHandler = (lang) => {
-        changeLocale(lang, null)
-    }
-
-    return (
-        <>
-        {currentLang ? 
-        <div className={[langStyles.wrapper, direction === 'up' ? langStyles.up : direction === 'down' ? langStyles.down : null].join(' ')} style={styling}>
-            <p className={[langStyles.currLang, "text-bold no-mb text-small"].join(' ')}>{ currentLangLabel }</p>
-            <ul className={["list-unstyled", langStyles.otherLangs].join(' ')}>
-                {langs.map((l, i) => currentLang !== l.lang ? <li key={i} onClick={() => langSwitchHandler(l.lang)}>{ l.label }</li> : null )}
-            </ul>
-        </div>
-        : null }
-        </>
-    )
-}
-
-export default LangSwitcher
+// export default LanguageSwitcher
