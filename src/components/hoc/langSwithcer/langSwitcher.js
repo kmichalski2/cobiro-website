7896d9c16a4b1f9017965e9898c6022c97705c45
Malthe Blichfeldt <maltheblichfeldt@gmail.com>
2020-02-20 11:50:45 +0100

    langSwich component

--- src/components/hoc/langSwithcer/langSwitcher.js
+++ src/components/hoc/langSwithcer/langSwitcher.js
@@ -1,37 +1,37 @@
-// import React, { useState, useEffect } from "react"
-// import { changeLocale, useIntl } from 'gatsby-plugin-intl'
+import React, { useState, useEffect } from "react"
+import { changeLocale, useIntl } from 'gatsby-plugin-intl'
 
-// import langStyles from './langSwitcher.module.scss'
+import langStyles from './langSwitcher.module.scss'
 
-// const LangSwitcher = ({ direction, styling }) => {
-//     const langs = [
-//         {lang: 'da', label: 'DK'},
-//         {lang: 'nb', label: 'NO'},
-//         {lang: 'en', label: 'UK'}
-//     ]
-//     const currentLang = useIntl().locale
-//     const [currentLangLabel, setCurrentLangLabel] = useState('')
+const LangSwitcher = ({ direction, styling }) => {
+    const langs = [
+        {lang: 'en', label: 'UK'},
+        {lang: 'da', label: 'DK'}
+    ]
+    
+    const currentLang = useIntl().locale
+    const [currentLangLabel, setCurrentLangLabel] = useState('')
 
-//     useEffect(() => {
-//         setCurrentLangLabel(currentLang === 'da' ? 'DK' : currentLang === 'nb' ? 'NO' : currentLang === 'en' ? 'UK' : '')
-//     })
+    useEffect(() => {
+        setCurrentLangLabel(currentLang === 'da' ? 'DK' : currentLang === 'nb' ? 'NO' : currentLang === 'en' ? 'UK' : '')
+    })
 
-//     const langSwitchHandler = (lang) => {
-//         changeLocale(lang, null)
-//     }
+    const langSwitchHandler = (lang) => {
+        changeLocale(lang, null)
+    }
 
-//     return (
-//         <>
-//         {currentLang ? 
-//         <div className={[langStyles.wrapper, direction === 'up' ? langStyles.up : direction === 'down' ? langStyles.down : null].join(' ')} style={styling}>
-//             <p className={[langStyles.currLang, "text-bold no-mb text-small"].join(' ')}>{ currentLangLabel }</p>
-//             <ul className={["list-unstyled", langStyles.otherLangs].join(' ')}>
-//                 {langs.map((l, i) => currentLang !== l.lang ? <li key={i} onClick={() => langSwitchHandler(l.lang)}>{ l.label }</li> : null )}
-//             </ul>
-//         </div>
-//         : null }
-//         </>
-//     )
-// }
+    return (
+        <>
+        {currentLang ? 
+        <div className={[langStyles.wrapper, direction === 'up' ? langStyles.up : direction === 'down' ? langStyles.down : null].join(' ')} style={styling}>
+            <p className={[langStyles.currLang, "text-bold no-mb text-small"].join(' ')}>{ currentLangLabel }</p>
+            <ul className={["list-unstyled", langStyles.otherLangs].join(' ')}>
+                {langs.map((l, i) => currentLang !== l.lang ? <li key={i} onClick={() => langSwitchHandler(l.lang)}>{ l.label }</li> : null )}
+            </ul>
+        </div>
+        : null }
+        </>
+    )
+}
 
-// export default LangSwitcher
+export default LangSwitcher
