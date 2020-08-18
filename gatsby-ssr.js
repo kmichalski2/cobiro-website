f07f5fc29696cc26fa6d7a630706fde2dee309c5
Malthe Blichfeldt <mb@mamacph.dk>
2020-08-18 14:02:24 +0200

    clean up

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,24 +3,7 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
-
-// You can delete this file if you're not using it
-
-// import React from "react";
-
-// export function onRenderBody(
-//   { setHeadComponents, setPreBodyComponents }
-// ) {
-//  setHeadComponents([
-//     <script src="https://apis.google.com/js/platform.js" async defer></script>
-//   ]);
-//   setPreBodyComponents([
-//     <script src="https://apis.google.com/js/platform.js" async defer></script>
-// ]);
-// }
-
 import React from "react";
-
 
 export const onRenderBody = ({ setPostBodyComponents }) => {
         setPostBodyComponents([
