bd088332c1f9d267d006f71a1031458b68224890
Mads Røjkum <roejkum@gmail.com>
2021-02-11 14:08:49 +0100

    Menu finalization

--- src/components/layout/navbar/subMenu/subMenu.js
+++ src/components/layout/navbar/subMenu/subMenu.js
@@ -66,12 +66,14 @@ const SubMenu = ({ footer, columns, columnRight, submenuTitle, submenuDescriptio
     return (
         <div className={[Classes.subMenu, show ? Classes.show : null, expand ? Classes.expand : null].join(' ')} ref={submenu} style={{marginLeft: `${offset}px`}}>
             <div className={Classes.subMenuInner}>
-              <div>{submenuTitle || submenuDescription ? 
-                <div className="space-xs-up">
+            {submenuTitle || submenuDescription ? 
+                <div className="space-xs-up" style={{width: '100%', flex: '2'}}>
                   {submenuTitle ? <h3>{ submenuTitle }</h3> : null}
                   {submenuDescription ? <HtmlText RawHtml={submenuDescription} classes="small  hidden-xs hidden-sm hidden-md" /> : null }
                 </div>
                 : null}
+                <div className="flex-lg">
+              <div>
                 <div className="flex-lg">
                   {columns && columns.length > 0 ? columns.map((sub, index) => (
                       <SubMenuColumn 
@@ -88,23 +90,23 @@ const SubMenu = ({ footer, columns, columnRight, submenuTitle, submenuDescriptio
               </div>
               {columnRight.subMenuTitle || columnRight.links.length > 0 ?
               <div className={Classes.borderLeft}>
-                <div className={["space-xs-up", Classes.colRightTitle].join(' ')}>
-                  <h3>{ columnRight.title }</h3>
-                  <HtmlText RawHtml={columnRight.description} classes={["small hidden-xs hidden-sm hidden-md", Classes.columnRightSpacing].join(' ')} />
-                </div>
                   <SubMenuColumn 
                     title={columnRight.subMenuTitle}
                     icon={columnRight.subMenuIcon}
+                    logo={columnRight.logo}
                     submenuLinks={columnRight.links}
                     expandedDefault={!columnRight.subMenuTitle && true}
                     borderLeft
                   />
               </div>
               : null}
+              </div>
                 </div>
                 { footer.submenuFooterText || (footer.submenuFooterLinkTitle && (footer.submenuFooterLink || footer.submenuFooterExternalLink)) ?
                 <SubMenuFooter 
+                  title={footer.submenuFooterTitle}
                   text={footer.submenuFooterText}
+                  image={footer.submenuFooterImage}
                   externalLink={footer.submenuFooterExternalLink}
                   internalLink={footer.submenuFooterLink && footer.submenuFooterLink.slug}
                   linkTitle={footer.submenuFooterLinkTitle}
