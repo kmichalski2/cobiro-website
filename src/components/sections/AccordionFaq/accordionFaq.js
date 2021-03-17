6b784ca9bb2f8c5784c282d14af283d3101b1dbd
Malthe Blichfeldt <mb@mamacph.dk>
2021-03-17 13:47:15 +0100

    Done

--- src/components/sections/AccordionFaq/accordionFaq.js
+++ src/components/sections/AccordionFaq/accordionFaq.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import AccordionStyles from './AccordionFaq.module.scss'
+import Classes from './AccordionFaq.module.scss'
 
 const AccordionFaq = ({ data }) => {
   const accordions = data.accordion.accordionTabs
@@ -24,13 +24,13 @@ const AccordionFaq = ({ data }) => {
       setExpandedAccordion(i)
       if (parentNode.classList.contains("accordion-header")) {
         setAccordionMaxHeight(parentNode.nextElementSibling.scrollHeight)
-      } else if(sibling && sibling.classList.contains(AccordionStyles.accordionText)) {
+      } else if(sibling && sibling.classList.contains(Classes.accordionText)) {
         setAccordionMaxHeight(sibling.scrollHeight)
-      } else if(parentNode.classList.contains(AccordionStyles.accordionText)) {
+      } else if(parentNode.classList.contains(Classes.accordionText)) {
         setAccordionMaxHeight(parentNode.scrollHeight)
       } else if (event.target.classList.contains("card")) {
         setAccordionMaxHeight(event.target.childNodes[1].scrollHeight)
-      } else if(event.target.classList.contains(AccordionStyles.accordionText)) {
+      } else if(event.target.classList.contains(Classes.accordionText)) {
         setAccordionMaxHeight(event.target.scrollHeight)
       }
       
@@ -39,28 +39,27 @@ const AccordionFaq = ({ data }) => {
 
 
   return (
-    <section className={[data.backgroundColor ? "bg-sway" : null, "section"].join(' ')}>
+    <section className={[data.backgroundColor ? "bg-extra-ligt-grey" : null, "section"].join(' ')}>
       <div className={data.backgroundColor ? "bg-sway-inner" : null}>
-        <div className="container">
+        <div className="container center">
           <div className="row">
             <div className="col col-xs-12 text-center section-header">
               { data.title ? <h2>{data.title}</h2> : null }
               { data.text ? <p>{data.text}</p> : null }
             </div>
-            <div className={["col col-xs-12", AccordionStyles.accordion].join(' ')}>
+            <div className={["col col-xs-12", Classes.accordion].join(' ')}>
               {accordions.map((acc, index) => (
-              <div key={index} className={["card card-visible text-left", expandedAccordion === index ? "expanded-accordion" : null].join(' ')} onClick={event => newAccordionClickHandler(event, index)}>
+              <div key={index} className={["card card-visible text-left", Classes.question, expandedAccordion === index ? "expanded-accordion" : null].join(' ')} onClick={event => newAccordionClickHandler(event, index)}>
                 <div className={["flex between-xs middle-xs accordion-header"]}>
                   <h5>{acc.title}</h5>
                   <button
-                    className={["btn btn-accordion btn-toggle btn-secondary", expandedAccordion === index ? "active" : null].join(' ')}
-                    
+                    className={["btn btn-toggle btn-unstyled btn-unstyled btn-accordion", expandedAccordion === index ? "active" : null].join(' ')}
                     aria-label="Expand accordion panel"
                   >
                     <span>+</span>
                   </button>
                 </div>
-                <div className={["small", AccordionStyles.accordionText, expandedAccordion === index ? AccordionStyles.expandedAccordion : null].join(' ')} dangerouslySetInnerHTML={createMarkup(acc.text)} style={expandedAccordion === index ? {maxHeight: accordionMaxHeight + "px"} : null}>
+                <div className={["small", Classes.accordionText, expandedAccordion === index ? Classes.expandedAccordion : null].join(' ')} dangerouslySetInnerHTML={createMarkup(acc.text)} style={expandedAccordion === index ? {maxHeight: accordionMaxHeight + "px"} : null}>
                 </div>
               </div> 
               ))}      
